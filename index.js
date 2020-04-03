3badb4cb1947aaf5e5c0409f250d2ad408d4d4af
Jannu Sudhakar <sudhajannu95@gmail.com>
2020-04-03 09:30:20 +0000

    swithched to DialoGPT

--- index.js
+++ index.js
@@ -13,24 +13,33 @@ io.sockets.on('connection', function(socket) {
         socket.username = username;
         socket.chat_history = [];
         const message = 'hi <i>' + socket.username + '</i>';
-        socket.chat_history.push({'speaker':'Joey','message':message});
-        socket.emit('chat_message', '<strong>Joey: </strong>' + message);
+        socket.chat_history.push({'speaker':'Neptune','message':message});
+        socket.emit('chat_message', '<strong>Neptune: </strong>' + message);
     });
 
     socket.on('chat_message', function(message) {
         socket.chat_history.push({'speaker':'user','message':message})
         socket.emit('chat_message', '<strong>' + socket.username + '</strong>: ' + message);
 
-        let feed_string = '';
+        let feed_string = ['./dialogpt_txtgen.py'];
+	let speaker = 'Neptune';
+	let curr_string = ''
         for (let i = 0; i < socket.chat_history.length; i++){
-          feed_string += socket.chat_history[i]['speaker'] + ': ' + socket.chat_history[i]['message'] + '\n';
+            if(speaker === socket.chat_history[i]['speaker']){
+		curr_string = curr_string + socket.chat_history[i]['message'];
+	    }
+	    else{
+		feed_string.push(curr_string);
+		curr_string = socket.chat_history[i]['message'];
+		speaker = socket.chat_history[i]['speaker'];
+	  }
         }
-        feed_string += 'Joey: ';
+        feed_string.push(curr_string);
 
-        const py = spawn('python3',['./gpt_txtgen.py',feed_string]);
+        const py = spawn('python3',feed_string);
         py.stdout.on('data',function(data){
-          socket.chat_history.push({'speaker':'Joey','message':data});
-          socket.emit('chat_message','<strong>Joey: </strong>' + data);
+          socket.chat_history.push({'speaker':'Neptune','message':data});
+          socket.emit('chat_message','<strong>Neptune: </strong>' + data);
         })
         py.stderr.on('data',function(data){
           socket.chat_history.push({'speaker':'errormsg','message':data});
@@ -40,6 +49,6 @@ io.sockets.on('connection', function(socket) {
 
 });
 
-const server = http.listen(8080, function() {
-    console.log('listening on *:8080');
+const server = http.listen(80, function() {
+    console.log('listening on *:80');
 });
